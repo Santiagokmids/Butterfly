015ad7c3348e40ce15aa8b3eb0f70ab8c1ff09a7
Santiago <sntgtrochez@gmail.com>
2021-11-20 21:59:02 -0500

    Dijstra are better but not finish

--- src/dataStructure/MatrixGraph.java
+++ src/dataStructure/MatrixGraph.java
@@ -1,7 +1,6 @@
 package dataStructure;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -57,18 +56,28 @@ public class MatrixGraph<V extends Comparable <V>, U, H> implements IMatrixGraph
 
 	@Override
 	public ArrayList<H> dijkstra(Vertice<V, U, H> start, Vertice<V, U, H> Final) {
+		
 		ArrayList<Integer> dist = new ArrayList<>();
-		ArrayList<V> nameDist = new ArrayList<>();
 		ArrayList<V> prev = new ArrayList<>();
-		Queue<V> queue = new LinkedList<>();
+		Queue<Vertice<V, U, H>> queue = new LinkedList<>();
+		ArrayList<Vertice<V, U, H>> reference = new ArrayList<>();
 
 		dist = assignSource(start, dist);
 		prev = assignPrev(prev);
 		queue = assignQueue(queue);
-		nameDist = assignNames(start, nameDist);
+		reference = assignRef();
 
 		while(!queue.isEmpty()) {
-
+			int height = 0;
+			queue = extractMin(queue, dist);
+			Vertice<V, U, H> value = queue.poll();
+			int index = searchReference(value, reference);
+			
+			for (int i = 0; i < value.getEdge().size(); i++) {
+				int finalVertice = searchReference(value.getEdge().get(index).getFinalVertice(), reference);
+				height +=  + (Integer)value.getEdge().get(index).getHeight();
+				
+			}
 		}
 
 		return null;
@@ -97,53 +106,71 @@ public class MatrixGraph<V extends Comparable <V>, U, H> implements IMatrixGraph
 		return prev;
 	}
 
-	private Queue<V> assignQueue(Queue<V> queue) {
+	private Queue<Vertice<V, U, H>> assignQueue(Queue<Vertice<V, U, H>> queue) {
 
 		for (int i = 0; i < vertice.size(); i++) {
-			queue.add(vertice.get(i).getValue());
+			queue.add(vertice.get(i));
 		}
 
 		return queue;
 	}
 
-	private ArrayList<V> assignNames(Vertice<V, U, H> start, ArrayList<V> names) {
-
-		for (int i = 0; i < vertice.size(); i++) {
-
-			if(start.getValue().compareTo(vertice.get(i).getValue()) == 0) {
-				names.add(0, vertice.get(i).getValue());
-
-			}else {
-				names.add(vertice.get(i).getValue());
-			}
+	private Queue<Vertice<V, U, H>> extractMin(Queue<Vertice<V, U, H>> queue, ArrayList<Integer> dist) {
+		
+		ArrayList<Vertice<V, U, H>> values = new ArrayList<>();
+		
+		for (int i = 0; i < dist.size(); i++) {
+			values.add(queue.poll());
 		}
-
-		return names;
-	}
-
-	private ArrayList<V> sortDist(ArrayList<V> names, ArrayList<Integer> dist) {
-
+		
 		for(int i = 0;i < dist.size();i++) {
 			int min = dist.get(i);
+			Vertice<V, U, H> minim = values.get(i);
 			
 			for(int j = i+1;j < dist.size();j++) {
 				
 				if(dist.get(j) < min) {
 					int temp = dist.get(j);
+					Vertice<V, U, H> temporal = values.get(j);
 					dist.set(j, min);
+					values.set(j, minim);
 					min = temp;
+					minim = temporal;
 				}
 			}
 			dist.set(i, min);
+			values.set(i, minim);
 		}
-		return names;
+		
+		queue.clear();
+		
+		for (int i = 0; i < values.size(); i++) {
+			queue.add(values.get(i));
+		}
+		return queue;
 	}
-
-	private V extractMin(ArrayList<V> names,Queue<V> queue) {
-
-		return null;
+	
+	private ArrayList<Vertice<V, U, H>> assignRef(){
+		
+		ArrayList<Vertice<V, U, H>> ref = new ArrayList<Vertice<V, U, H>>();
+		
+		for (int i = 0; i < vertice.size(); i++) {
+			ref.add(vertice.get(i));
+		}
+		return ref;
 	}
-
+	
+	private int searchReference(Vertice<V, U, H> vert, ArrayList<Vertice<V, U, H>> reference) {
+		int index = 0;
+		
+		for (int i = 0; i < reference.size(); i++) {
+			if(reference.get(i) == vert) {
+				index = i;
+			}
+		}
+		return index;
+	}
+	
 	@Override
 	public void floyd() {
 		// TODO Auto-generated method stub
