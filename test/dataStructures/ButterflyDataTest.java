5442aa471668a1c1f91ec09f90a8aa10ae29e846
Gianni <70019070+G20-00@users.noreply.github.com>
2021-11-23 18:34:19 -0500

    change test

---
+++ test/dataStructures/ButterflyDataTest.java
@@ -0,0 +1,23 @@
+package dataStructures;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import dataStructure.MatrixGraph;
+
+
+
+class ButterflyDataTest<V extends Comparable <V>, U, H extends Comparable<H>>{
+	private MatrixGraph<V, U, H> mg;
+	
+	public void setupScenary1() {
+		mg = new MatrixGraph<V, U,H>();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
